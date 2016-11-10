03e1390e155d79554dafe250de12095bb3b5c269
Zan Markan <zan@pusher.com>
2016-11-10 10:12:17 +0000

    Simplified the GCM token retrieval logic and removed Guava

--- Library/src/main/java/com/pusher/android/notifications/gcm/GCMRegistrationIntentService.java
+++ Library/src/main/java/com/pusher/android/notifications/gcm/GCMRegistrationIntentService.java
@@ -5,16 +5,11 @@ import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-import com.github.rholder.retry.RetryException;
-import com.github.rholder.retry.Retryer;
-import com.github.rholder.retry.RetryerBuilder;
-import com.github.rholder.retry.StopStrategies;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
 import com.pusher.android.notifications.PushNotificationRegistration;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
+import java.io.IOException;
 
 /**
  * Created by jamiepatel on 10/06/2016.
@@ -22,7 +17,6 @@ import java.util.concurrent.ExecutionException;
 public class GCMRegistrationIntentService extends IntentService {
     private static final String TAG = "PusherRegIntentService";
     private static final Integer INSTANCE_ID_RETRY_ATTEMPTS = 10;
-
 
     public GCMRegistrationIntentService() {
         super(TAG);
@@ -33,38 +27,24 @@ public class GCMRegistrationIntentService extends IntentService {
 
         final InstanceID instanceID = InstanceID.getInstance(this);
         final String defaultSenderId = intent.getStringExtra("gcm_defaultSenderId");
-        String token;
-
-        Callable<String> tokenRetrieval = new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                return instanceID.getToken(defaultSenderId, GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
-            }
-        };
-
-        Retryer<String> retryer = RetryerBuilder.<String>newBuilder().
-                retryIfRuntimeException().
-                withStopStrategy(StopStrategies.stopAfterAttempt(INSTANCE_ID_RETRY_ATTEMPTS)).
-                build();
 
         Intent gcmCalled = new Intent(PushNotificationRegistration.GCM_CALLED_INTENT_FILTER);
+        int retryAttempts = 0;
+        String token = null;
 
-        try {
-            token = retryer.call(tokenRetrieval);
-        } catch (ExecutionException e) {
-            Log.e(TAG, e.getMessage());
-            return;
-        } catch (RetryException e) {
-            Log.e(TAG,
-                    "Failed to get token after " +
-                            INSTANCE_ID_RETRY_ATTEMPTS +
-                            " :" +
-                    e.getMessage());
-            token = null;
+        while (token == null && retryAttempts < INSTANCE_ID_RETRY_ATTEMPTS){
+            try {
+                token = instanceID.getToken(defaultSenderId, GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            } catch (IOException e) {
+                retryAttempts++;
+            }
+        }
+
+        if(token == null){
+            Log.e(TAG, "Failed to get token after " + INSTANCE_ID_RETRY_ATTEMPTS + " attempts.");
         }
 
         gcmCalled.putExtra(PushNotificationRegistration.TOKEN_EXTRA_KEY, token);
         LocalBroadcastManager.getInstance(this).sendBroadcast(gcmCalled);
     }
-
 }
